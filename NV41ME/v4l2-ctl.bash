3a6fa84aa498ac2e7c7bfe36076d891269376106
Mavin <vincenzo.marragony@gmail.com>
2021-01-29 23:14:34 +0100

    feat: v2l4-ctl

---
+++ NV41ME/v4l2-ctl.bash
@@ -0,0 +1,7 @@
+sudo apt install -y v4l-utils
+
+v4l2-ctl -c brightness=160,contrast=32,saturation=70,\
+hue=9,white_balance_temperature_auto=0,gamma=120,\
+gain=4,power_line_frequency=2,white_balance_temperature=4600,\
+sharpness=7,backlight_compensation=0,exposure_auto=3,\
+exposure_absolute=156,exposure_auto_priority=1
