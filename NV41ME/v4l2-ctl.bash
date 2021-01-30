476d3af070960ce4ab41cbb1204a743cb5118009
Mavin <vincenzo.marragony@gmail.com>
2021-01-30 14:17:01 +0100

    feat: loop until settings are ok

--- NV41ME/v4l2-ctl.bash
+++ NV41ME/v4l2-ctl.bash
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-v4l2-ctl -c brightness=160,contrast=32,saturation=70,\
+TARGET_BRIGHTNESS=160
+BRIGHTNESS=$(v4l2-ctl -C brightness)
+
+while [ "$BRIGHTNESS" != "brightness: $TARGET_BRIGHTNESS" ]
+do
+v4l2-ctl -c brightness=$TARGET_BRIGHTNESS,contrast=32,saturation=70,\
 hue=9,white_balance_temperature_auto=0,gamma=120,\
 gain=4,power_line_frequency=2,white_balance_temperature=4600,\
 sharpness=7,backlight_compensation=0,exposure_auto=3,\
 exposure_absolute=156,exposure_auto_priority=1
+
+  sleep 2
+
+  BRIGHTNESS=$(v4l2-ctl -C brightness)
+done
+
+
