5904e5fdae07309d2c099c73ccc7bd0a346f9194
Mavin <vincenzo.marragony@gmail.com>
2021-01-30 14:05:40 +0100

    feat: crontab to systemd service

--- NV41ME/v4l2-setup.bash
+++ NV41ME/v4l2-setup.bash
@@ -7,16 +7,12 @@ CURRENT_PATH=$(realpath $0)
 CURRENT_DIR=$(dirname $CURRENT_PATH)
 
 V4L2_CONFIGURE=$CURRENT_DIR/v4l2-ctl.bash
+V4L2_SERVICE=$CURRENT_DIR/v4l2.service
+V4L2_SYSTEMD_PATH=/etc/systemd/system/v4l2.service
 
 TEMP_FILE=$(mktemp)
 
 bash $V4L2_CONFIGURE
 
-crontab -l > $TEMP_FILE
-
-# TODO check crontab doesn't already have V4L2 configuration
-
-echo "@reboot /bin/bash $V4L2_CONFIGURE" >> $TEMP_FILE
-
-crontab $TEMP_FILE
-rm $TEMP_FILE
+sudo cp -f $V4L2_SERVICE $V4L2_SYSTEMD_PATH
+sudo sed -i "s+:script+$V4L2_CONFIGURE+g" $V4L2_SYSTEMD_PATH
