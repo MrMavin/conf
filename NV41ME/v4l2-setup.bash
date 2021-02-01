3316728788716e9c9eab1dd3574b46dac5e81a83
Mavin <vincenzo.marragony@gmail.com>
2021-02-01 09:32:44 +0100

    fix: removed unused TEMP_FILE

--- NV41ME/v4l2-setup.bash
+++ NV41ME/v4l2-setup.bash
@@ -10,8 +10,6 @@ V4L2_CONFIGURE=$CURRENT_DIR/v4l2-ctl.bash
 V4L2_SERVICE=$CURRENT_DIR/v4l2.service
 V4L2_SYSTEMD_PATH=/etc/systemd/system/v4l2.service
 
-TEMP_FILE=$(mktemp)
-
 bash $V4L2_CONFIGURE
 
 sudo cp -f $V4L2_SERVICE $V4L2_SYSTEMD_PATH
