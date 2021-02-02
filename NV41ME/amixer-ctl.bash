e5c32d672be75797839f8d4d11e6d17f82f24c58
Mavin <vincenzo.marragony@gmail.com>
2021-02-02 09:15:38 +0100

    fix: amixer avoid to set mic volume every second

--- NV41ME/amixer-ctl.bash
+++ NV41ME/amixer-ctl.bash
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+
 while [ true ]
 do
-  amixer sset "Capture" 32 >> /dev/null
-  amixer sset "Mic Boost" 0 >> /dev/null
-  amixer sset Digital 0 >> /dev/null
+  amixer sget "Capture" | grep 32 >> /dev/null || amixer sset "Capture" 32 >> /dev/null
+
+  AMIXER_MIC_BOOST=$(amixer sget "Mic Boost" | grep "Front")
+  AMIXER_DIGITAL=$(amixer sget "Digital" | grep "Front")
+
+  if [[ $AMIXER_MIC_BOOST != *"[0%]"* ]]; then
+      amixer sset "Mic Boost" 0 >> /dev/null
+  fi
+
+
+  if [[ $AMIXER_DIGITAL != *"[0%]"* ]]; then
+      amixer sset Digital 0 >> /dev/null
+  fi
 
   sleep 1
 done
