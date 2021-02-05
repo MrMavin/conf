23ef0de6506d68426038211f62b3b55b7560e682
Mavin <vincenzo.marragony@gmail.com>
2021-02-05 18:58:17 +0100

    feat: updated v4l2 configuration

--- NV41ME/v4l2-ctl.bash
+++ NV41ME/v4l2-ctl.bash
@@ -7,7 +7,7 @@ while [ "$BRIGHTNESS" != "brightness: $TARGET_BRIGHTNESS" ]
 do
 v4l2-ctl -c brightness=$TARGET_BRIGHTNESS,contrast=32,saturation=70,\
 hue=9,white_balance_temperature_auto=1,gamma=120,\
-gain=4,power_line_frequency=2,white_balance_temperature=2800f,\
+gain=4,power_line_frequency=2,white_balance_temperature=4000f,\
 sharpness=5,backlight_compensation=0,exposure_auto=3,\
 exposure_absolute=156,exposure_auto_priority=1
 
